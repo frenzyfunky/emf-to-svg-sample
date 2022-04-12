2509f01b87052e91c4e248741cebe1cf84aee7de
Yagiz Yorulmazlar <yyorulmazlar@gmail.com>
2022-04-12 09:02:30 +0300

    first commit

---
+++ EMFtoSVGConverter/Program.cs
@@ -0,0 +1,27 @@
+﻿using Aspose.Imaging;
+using Aspose.Imaging.ImageOptions;
+using System;
+
+namespace EMFtoSVGConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var emfPath = "example.emf";
+            var destPath = "svg_out.svg";
+
+            using (Aspose.Imaging.Image image = Aspose.Imaging.Image.Load(emfPath))
+            {
+                // Initilaize EmfRasterizationOptions class object
+                EmfRasterizationOptions emfRasterizationOptions = new EmfRasterizationOptions();
+
+                emfRasterizationOptions.BackgroundColor = Color.Transparent;
+                emfRasterizationOptions.PageWidth = image.Width;
+                emfRasterizationOptions.PageHeight = image.Height;
+
+                image.Save(destPath, new SvgOptions() { VectorRasterizationOptions = emfRasterizationOptions });
+            }
+        }
+    }
+}
